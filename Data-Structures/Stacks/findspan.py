cc1302a4e4560e38fe610507c1b70dbf880e59bd
shubhamg2404 <shubhamg2404@gmail.com>
2019-02-22 13:54:45 +0530

    implemented finding span problem

---
+++ Data-Structures/Stacks/findspan.py
@@ -0,0 +1,35 @@
+from stack import Stack
+
+def find_span(array):
+    
+    span_array = [];
+    day_stack = Stack()
+    for index,day_value in enumerate(array):
+        if index ==0:
+            day_stack.push(index)
+            span_array.append(0)
+            continue
+        top = array[day_stack.get_top()]
+        while day_value > top and not day_stack.is_empty():
+            day_stack.pop()
+            top = array[day_stack.get_top()]
+           
+        if not day_stack.is_empty():
+            top = day_stack.get_top()
+            span_array.append(index - top)
+            day_stack.push(index)
+            #print("day {} value {} top {}".format(index,day_value,array[top]))
+            #print(day_stack)
+        else:
+            #print("In else for day {} value {}".format(index,day_value))
+            span_array.append(0)
+            span_array.append(index)
+            
+    return span_array
+            
+        
+
+
+def main():
+    print(find_span([100,80,60,70,60,75,85]))
+main()
---
+++ Data-Structures/Stacks/stackutils.py
@@ -0,0 +1,23 @@
+from stack import Stack
+
+def reverse_stack(stack):
+    x = None
+    if not stack.is_empty():
+        x = stack.pop().data
+        print(x)
+        reverse_stack(stack)
+        stack.push(x)
+        return stack
+
+def main():
+    stack = Stack()
+    for i in xrange(10):
+        stack.push(i)
+
+    print(stack)
+
+    print reverse_stack(stack)
+
+    
+    
+main()        
