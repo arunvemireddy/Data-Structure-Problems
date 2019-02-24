f7b6e8e9492a891697d69d0da3eded5e68f82038
shubham <shubhamg2404@gmail.com>
2019-02-25 01:08:31 +0530

    implemented queue with stacks

---
+++ Data-Structures/Queue/queue-with-stack.py
@@ -0,0 +1,27 @@
+from stack import Stack
+
+class QueueWithStack:
+    def __init__(self):
+        self.first_stack = Stack()
+        self.second_stack = Stack()
+
+    def enqueue(self,data):
+        self.first_stack.push(data)
+
+    def dequeue(self):
+        if not self.second_stack.is_empty():
+            return self.second_stack.pop().data
+        else:
+            if self.first_stack.is_empty():
+                raise ValueError("UnderFlow")
+            while not self.first_stack.is_empty():
+                self.second_stack.push(self.first_stack.pop().data)
+            return self.second_stack.pop().data
+
+def main():
+    queue = QueueWithStack()
+    for i in range(1,10):
+        queue.enqueue(i)
+    for i in range(1,10):
+        print ( queue.dequeue () )
+main()
