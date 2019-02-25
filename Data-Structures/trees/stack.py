3ccdea5f7152dbd1281ec55644cfd4b343232a97
shubham <shubhamg2404@gmail.com>
2019-02-26 00:11:30 +0530

    implemeted tree

---
+++ Data-Structures/trees/stack.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self,data,nNode=None):
+        self.data = data
+        self.next = nNode
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+        
+    def push(self,data):
+        if self.head is None:
+            self.head = Node(data,None)
+            self.length +=1
+            return
+        new_node = Node(data,self.head)
+        self.head = new_node
+        self.length +=1
+    
+    def pop(self):
+        if self.length == 0:
+            raise ValueError("Under Flow Condition")
+        retValue = self.head
+        self.head = self.head.next
+        self.length -=1
+        return retValue
+
+    def __str__(self):
+        
+        message = ""
+        traverse = self.head
+        if traverse is None:
+            return "Empty Stack"
+        
+        while traverse is not None:
+            message += "{}|".format(traverse.data)
+            traverse = traverse.next
+        return message
+    
+    def is_empty(self):
+        if self.length == 0:
+            return True
+        return False
+    
+    def get_top(self):
+        if self.head is not None:
+            return self.head.data
+        print("Empty Stack")
+        return None
+            
+
+def main():
+    stack = Stack()
+    for i in range(10):
+        stack.push(i)
+    print(stack)
+    for i in range(11):
+        stack.pop()
+    print(stack)
+
+            
+        
---
+++ Data-Structures/trees/tree.py
@@ -0,0 +1,84 @@
+from stack import Stack
+
+class Node:
+    def __init__(self,data,left_node=None, right_node=None):
+        self.data = data
+        self.left = left_node
+        self.right = right_node
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.height = 0
+    def preorder_traverse(self):
+        self.__preorder_traverse(self.root)
+    def inorder_traverse(self):
+        self.__inorder_traverse(self.root)
+    def postorder_traverse(self):
+        self.__postorder_traverse(self.root)
+        
+    def preorder_traverse_itretive(self):
+        stack = Stack()
+        root = self.root
+        while True:
+            while root:
+                print(root.data)
+                stack.push(root)
+                root = root.left
+            if stack.is_empty():
+                break
+            root = stack.pop().data
+            root = root.right
+    def inorder_traverse_iterative(self):
+        stack = Stack()
+        root = self.root
+        while True:
+            while root:
+                stack.push(root)
+                root = root.left
+            if stack.is_empty():
+                break
+            root = stack.pop().data
+            print(root.data)
+            root = root.right
+    
+    def __preorder_traverse(self,node):
+        if(node is not None):
+            print(node.data),
+            self.__preorder_traverse(node.left)
+            self.__preorder_traverse(node.right)
+
+    def __inorder_traverse(self,node):
+        if(node is not None):
+            self.__inorder_traverse(node.left)
+            print(node.data)
+            self.__inorder_traverse(node.right)
+    def __postorder_traverse(self,node):
+        if(node is not None):
+            self.__inorder_traverse(node.left)
+            self.__inorder_traverse(node.right)
+            print(node.data)
+
+    def setup(self):
+        self.root = Node(1)
+        first_node = Node(2)
+        second_node = Node(3)
+        self.root.left = first_node
+        self.root.right = second_node
+        third_node = Node(4)
+        fourth_node = Node(5)
+        fifth_node = Node(6)
+        sixth_node = Node(7)
+        first_node.left = third_node
+        first_node.right = fourth_node
+        second_node.left = fifth_node
+        second_node.right = sixth_node
+        
+def main():
+    tree =Tree()
+    tree.setup()
+    #tree.preorder_traverse_itretive()
+    #tree.inorder_traverse()
+    #print('break')
+    #tree.inorder_traverse_iterative()
+    tree.postorder_traverse()
+main()
