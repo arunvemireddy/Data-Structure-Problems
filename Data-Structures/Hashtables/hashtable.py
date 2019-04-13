af5d83e3d26aa7b1d2d4f400c05f9dbf58549109
shubham <shubhamg2404@gmail.com>
2019-04-14 01:54:25 +0530

    ameliorated hashtables

--- Data-Structures/Hashtables/hashtable.py
+++ Data-Structures/Hashtables/hashtable.py
@@ -1,9 +1,10 @@
 class HashTable:
-    def __init__(self):
+    def __init__(self,probing='linear'):
         self.data = [None]*11
         self.length = 11
         self.filled = 0
         self.keys = []
+        self.probing = probing
     def __hash(self,key):
         if len(key) == 0:
             raise ValueError("Invalid key")
@@ -15,7 +16,8 @@ class HashTable:
     def insert(self,key,value):
         index = self.__hash(key)
         if self.filled == self.length:
-            raise ValueError("Hash Table is Full")
+            print("extanding")
+            self.expand()
         self.keys.append(key)
         self.prob(index,0,(key,value))
         self.filled +=1
@@ -24,7 +26,16 @@ class HashTable:
         if not self.data[index+factor]:
             self.data[index+factor] = data
             return True
-        self.prob(index,factor+1,data)
+        self.prob
+        (index,factor+1,data)
+
+    def quad_prob(self,index,factor,data):
+        if not self.data[index+factor]:
+            self.data[index+factor] = data
+            return True
+        self.prob(index,(factor+1)**2,data)
+    
+
     def search(self,key):
         if key not in self.keys:
             raise ValueError("Invalid key")
@@ -33,16 +44,37 @@ class HashTable:
             index +=1
         return self.data[index][1]
 
+    def delete(self,key):
+        pass
+
+    def expand(self):
+        new_list = [None]*(2*self.length)
+        temp = self.data
+        self.data = new_list
+        self.length = len(self.data)
+        for entry in temp:
+            if entry:
+                self.insert(entry[0],entry[1])
+    
+    def shrink(self):
+        if not self.filled <= self.length//4:
+            return
+        new_list = [None]*(self.length//2)
+        temp = self.data
+        self.data = new_list
+        self.length = len(self.data)
+        for entry in temp:
+            if entry:
+                self.insert(entry[0],entry[1])
+
+
     def __str__(self):
         return str(self.data)
 def main():
     table = HashTable()
-    table.insert("shubham",23)
-    table.insert("gupta",25)
-    table.insert("shikha",98)
-    table.insert("aditya",10)
-    table.insert("ayushi",89)
-    table.insert("arushi",98)
+    values = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o']
+    for i in range(15):
+        print(i)
+        table.insert(values[i],i)
     print(table)
-    print(table.search("shubham"))
 main()
